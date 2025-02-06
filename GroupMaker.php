d9815df6f231a7a8af33e8374c2958ce0f7171f1
CloudSalander <ruben.as532@gmail.com>
2025-02-06 10:53:36 +0100

    Entering students

--- GroupMaker.php
+++ GroupMaker.php
@@ -3,7 +3,21 @@ class GroupMaker {
     private array $students;
     private int $groupSize;
 
-    public function enterStudents(): void {}
+    const ENTER_STUDENTS_MSG = "Please, enter students one by one. Empty entry to stop";
+
+    public function __construct() {
+        $this->students = [];
+    }
+
+    public function enterStudents(): void {
+        $student = readline(self::ENTER_STUDENTS_MSG);
+        //TODO: We can improve input validation
+        while($student != "") {
+            array_push($this->students,$student);
+            $student = readline(self::ENTER_STUDENTS_MSG);
+        }
+        var_dump($this->students);
+    }
     public function enterGroupSize(): void {}
     public function generateGroups(): array {}
     public function showGroups(): void {}
--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include('GroupMaker.php');
 
 $groupMaker = new GroupMaker();
 
-var_dump($groupMaker);
+$groupMaker->enterStudents();
 
 ?>
