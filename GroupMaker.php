90bec5b1ee789438e7949775482c65f1c95c155d
CloudSalander <ruben.as532@gmail.com>
2025-02-06 20:54:18 +0100

    Now is generating groups

--- GroupMaker.php
+++ GroupMaker.php
@@ -2,17 +2,19 @@
 class GroupMaker {
     private array $students;
     private int $groupSize;
+    private array $groupedStudents;
 
     const ENTER_STUDENTS_MSG = "Please, enter students one by one. Empty entry to stop";
     const ENTER_GROUP_SIZE_MSG = "Please, enter group size";
 
     public function __construct() {
         $this->students = [];
+        $this->groupedStudents = [];
     }
 
     public function enterStudents(): void {
         $student = readline(self::ENTER_STUDENTS_MSG);
-        //TODO: We can improve input validation
+        //TODO: Input validation?
         while($student != "") {
             array_push($this->students,$student);
             $student = readline(self::ENTER_STUDENTS_MSG);
@@ -21,20 +23,22 @@ class GroupMaker {
     public function enterGroupSize(): void {
         //TODO: Validate this input
         $this->groupSize = intval(readline(self::ENTER_GROUP_SIZE_MSG));
-        $con = 0;
     }
-    public function generateGroups(): array {
-        $groups = [];
+    public function generateGroups(): void {
+        $this->groupedStudents = $this->generateEmptyGroups();
         $con = 0;
         foreach($this->students as $student) {
-            array_push($groups[$con%$this->groupSize],$student);
+            array_push($this->groupedStudents[$con%$this->groupSize],$student);
             ++$con;
         }
+        var_dump($this->groupedStudents);
     }
-    private function generateEmptyGroups() {
-        for($con = 0; $con < $this->groupSize;++$i) {
-            $this->groupSize 
+    private function generateEmptyGroups(): array {
+        $groups = [];
+        for($con = 0; $con < $this->groupSize; ++$con) {
+            $groups[$con] = [];
         }
+        return $groups;
     }
 }
 ?>
